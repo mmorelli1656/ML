e73c7d02de2b7208c5c9e29674ca3b12b96f31a8
mmorelli1656 <morellimichele1997@gmail.com>
2025-09-15 17:31:13 +0200

    chore: cleaning useless libreries

--- 02_ml_classification.py
+++ 02_ml_classification.py
@@ -9,11 +9,8 @@ Created on Sat Sep 13 16:42:29 2025
 
 import sys
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from joblib import cpu_count
+from sklearn.preprocessing import StandardScaler
 from pathlib import Path
 
 # Models
@@ -27,58 +24,13 @@ from sklearn.svm import SVC
 from Utils.project_paths import ProjectPaths
 from Utils.elapsed_timer import Timer
 # Machine learning
+from ML.feature_analysis_viz import variance_histogram, correlation_histogram
 from ML.my_featsel import FeaturesVariance, FeaturesPearson
 from ML.my_parallel_ML import ParallelModelTrainer
+# from ML.parallel_model_trainer import ParallelModelTrainer
 from ML.my_eval import EvaluationMetrics
 
 sys.path.append(str(Path.cwd().resolve() / "ML"))
-
-
-#%% Functions
-
-def variance_histogram(X, bins, percentile=None):
-    """
-    Applica Min-Max scaling al DataFrame X, calcola la varianza per colonna
-    e mostra un istogramma della distribuzione delle varianze.
-    
-    Se specificato, aggiunge una linea verticale per il valore di un percentile.
-
-    Parametri:
-        X: DataFrame originale
-        bins: numero di intervalli dell'istogramma
-        percentile: valore percentile da evidenziare (0-100) oppure None
-
-    Ritorna:
-        X_scaled: DataFrame scalato
-        variances: Series con varianze delle colonne
-    """
-    # Copia e scala
-    X_scaled = X.copy()
-    scaler = MinMaxScaler()
-    X_scaled[:] = scaler.fit_transform(X_scaled)
-
-    # Calcola varianza
-    variances = X_scaled.var()
-
-    # Plot istogramma
-    plt.figure(figsize=(10, 6))
-    plt.hist(variances, bins=bins, color='skyblue', edgecolor='black')
-
-    # Percentile opzionale
-    if percentile is not None and 0 <= percentile <= 100:
-        perc_value = np.percentile(variances, percentile)
-        plt.axvline(perc_value, color='red', linestyle='--', linewidth=2,
-                    label=f'{percentile}° percentile = {perc_value:.4f}')
-        plt.legend(fontsize=12)
-
-    plt.title('Distribuzione delle varianze', fontsize=16)
-    plt.xlabel('Varianza', fontsize=14)
-    plt.ylabel('N° features', fontsize=14)
-    plt.grid(True, linestyle='--', alpha=0.5)
-    plt.tight_layout()
-    plt.show()
-
-    return variances
 
 
 #%% Load files
@@ -93,20 +45,22 @@ proc_path = paths.get_datasets_path(processed=True)
 X = pd.read_parquet(proc_path / "data.parquet")
 
 # Extract label
-y = X.pop("Truth")
-y.name = "Label"    # rinomina la Series in "label"
+y = X.pop("Label")
 
 del proc_path
 
 
-#%% Istogramma delle varianze
+#%% Feature properties histograms
 
-_ = variance_histogram(X, bins='auto', percentile=50)
+# Varaince histogram
+_ = variance_histogram(X, bins='auto', percentile=60)
+# Pearson correlation histogram
+_ = correlation_histogram(X, bins='auto', threshold=0.80)
 
 
 #%% Pipeline
 
-# Inizializza la CV
+# 
 rskf = RepeatedStratifiedKFold(
     n_splits=20,        # numero di fold
     n_repeats=100,       # numero ripetizioni
@@ -116,8 +70,6 @@ rskf = RepeatedStratifiedKFold(
 # Inizializza i metodi di FS
 feature_selectors = [FeaturesVariance(threshold_value=60, mode='percentile'),
                      FeaturesPearson(threshold=0.80, alpha=0.01, random_state=42)]
-
-feature_selectors = [FeaturesPearson(threshold=0.80, alpha=0.01, random_state=42)]
 
 # Inizializza lo scaler
 scaler = StandardScaler()
@@ -145,28 +97,7 @@ model = xgb.XGBClassifier(
 )
 
 
-# Parametri XGB per 60% con M>=5
-# model = xgb.XGBClassifier(
-#     eval_metric='logloss',
-#     n_jobs=-1,
-#     random_state=42,
-#     early_stopping_rounds=20,
-#     colsample_bytree=0.60,      # quantità di feature da campionare per ogni albero
-#     # gamma=1,                   # regolarizzazione minima per uno split
-#     # learning_rate=0.1,         # alias: eta
-#     max_depth=3,               # profondità massima degli alberi
-#     n_estimators=100,          # numero di boosting rounds
-#     subsample=0.60             # percentuale di campionamento dei dati per albero
-# )
-
-
-# Inizializza il modello
-# model = LogisticRegression(max_iter=200)  # aumento max_iter se serve
-
-
 #%% Addestramento e risultati
-
-print(f"Numero di core logici disponibili: {cpu_count()}")
 
 # Inizializza l'addestramento parallelo
 trainer = ParallelModelTrainer(X, y, rskf, scaler, model, balancer=None,
@@ -190,10 +121,10 @@ list_df_pred_proba = trainer.get_predictions_proba(results)
 # save_results(final_results, results_path, save_scalers=False, save_models=False)
 
 # Conta quante features vengono prese in ogni split
-columns_with_ones_dict = {
-    idx: list(df_feat_sel.columns[row == 1])
-    for idx, row in df_feat_sel.iterrows()
-}
+# columns_with_ones_dict = {
+#     idx: list(df_feat_sel.columns[row == 1])
+#     for idx, row in df_feat_sel.iterrows()
+# }
 
 # del rskf, feature_selectors, scaler, model, trainer
 
